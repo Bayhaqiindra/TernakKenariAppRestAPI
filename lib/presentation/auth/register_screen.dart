b9da0947b232378bc17143bf04e3c70f50b2066b
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-06-06 21:03:00 +0700

    menambahkan role id

--- lib/presentation/auth/register_screen.dart
+++ lib/presentation/auth/register_screen.dart
@@ -23,7 +23,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
   late final TextEditingController passwordController;
   late final GlobalKey<FormState> _key;
   bool isShowPassword = false;
-
+  
   @override
   void initState() {
     namaController = TextEditingController();
@@ -139,6 +139,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                                     password: passwordController.text,
                                     passwordConfirmation:
                                         passwordController.text,
+                                        roleId: 2,
                                   );
 
                                   context.read<RegisterBloc>().add(
