734e4bf3e71251c76a31604ff12fae543e90037c
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-06-06 20:08:29 +0700

    Menambahkan tampilan daftar burung tersedia dengan detail dan fitur refresh

--- lib/presentation/buyer/home/buyer_home_screen.dart
+++ lib/presentation/buyer/home/buyer_home_screen.dart
@@ -64,4 +64,200 @@ class _BuyerHomeScreenState extends State<BuyerHomeScreen> {
           ),
         ],
       ),
-      
+      body: RefreshIndicator(
+        onRefresh: () async {
+          context.read<GetBurungTersediaBloc>().add(
+            GetAllBurungTersediaEvent(),
+          );
+        },
+        child: Column(
+          children: [
+            const SpaceHeight(10),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                "Daftar Burung Tersedia",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SpaceHeight(10),
+            // Search bar
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Cari burung...",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  prefixIcon: const Icon(Icons.search),
+                ),
+                onChanged: (value) {
+                  // Implement search functionality
+                },
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: BlocBuilder<
+                  GetBurungTersediaBloc,
+                  GetBurungTersediaState
+                >(
+                  builder: (context, state) {
+                    if (state is GetBurungTersediaLoading) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+
+                    if (state is GetBurungTersediaError) {
+                      return Center(
+                        child: Text("Terjadi kesalahan: ${state.message}"),
+                      );
+                    }
+
+                    if (state is GetBurungTersediaLoaded) {
+                      final List<DataBurungTersedia> burungList =
+                          state.burungTersedia.data;
+
+                      if (burungList.isEmpty) {
+                        return const Center(
+                          child: Text("Tidak ada burung tersedia."),
+                        );
+                      }
+
+                      return GridView.builder(
+                        itemCount: burungList.length,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                              mainAxisSpacing: 10,
+                              crossAxisSpacing: 10,
+                              childAspectRatio: 0.8,
+                            ),
+                        itemBuilder: (context, index) {
+                          final burung = burungList[index];
+
+                          return GestureDetector(
+                            onTap: () {
+                              showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return CupertinoAlertDialog(
+                                    title: const Text("Detail Burung"),
+                                    content: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text("No Ring: ${burung.noRing}"),
+                                        Text("Usia: ${burung.usia}"),
+                                        Text(
+                                          "Jenis Kenari: ${burung.jenisKenari}",
+                                        ),
+                                        Text(
+                                          "Jenis Kelamin: ${burung.jenisKelamin}",
+                                        ),
+                                        Text("Harga: Rp${burung.harga}"),
+                                        Text(
+                                          "Deskripsi: ${burung.deskripsi.isNotEmpty ? burung.deskripsi : 'Tidak ada deskripsi'}",
+                                        ),
+                                      ],
+                                    ),
+                                    actions: [
+                                      CupertinoDialogAction(
+                                        child: const Text("Tutup"),
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                    ],
+                                  );
+                                },
+                              );
+                            },
+                            child: Card(
+                              elevation: 4,
+                              color: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: const BorderRadius.vertical(
+                                      top: Radius.circular(12),
+                                    ),
+                                    child:
+                                        burung.image.isNotEmpty
+                                            ? Image.network(
+                                              burung.image,
+                                              height: 100,
+                                              width: double.infinity,
+                                              fit: BoxFit.fitHeight,
+                                            )
+                                            : Container(
+                                              height: 100,
+                                              width: double.infinity,
+                                              color: Colors.grey[300],
+                                              child: const Icon(
+                                                Icons.image_not_supported,
+                                              ),
+                                            ),
+                                  ),
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            burung.noRing,
+                                            style: const TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          Text(
+                                            "Jenis: ${burung.jenisKenari}",
+                                            overflow: TextOverflow.ellipsis,
+                                          ),
+                                          Text(
+                                            "Kelamin: ${burung.jenisKelamin}",
+                                            overflow: TextOverflow.ellipsis,
+                                          ),
+                                          Text(
+                                            "Harga: Rp${burung.harga}",
+                                            overflow: TextOverflow.ellipsis,
+                                          ),
+                                          const SizedBox(height: 4),
+                                          Text(
+                                            "Status: ${burung.status}",
+                                            style: const TextStyle(
+                                              color: Colors.green,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                            overflow: TextOverflow.ellipsis,
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    }
+                    return const SizedBox(); // default kosong
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
