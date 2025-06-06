e08afd70beae975d32dc053b1c169ac8a5169bc5
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-06-06 20:05:18 +0700

    Menambahkan ServiceHttpClient untuk request HTTP

---
+++ lib/service/service_http_client.dart
@@ -0,0 +1,65 @@
+import 'dart:convert';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+
+class ServiceHttpClient {
+  final String baseUrl = 'http://10.0.2.2:8000/api/';
+  final secureStorage = FlutterSecureStorage();
+
+  //POST
+  Future<http.Response> post(String endpoint, Map<String, dynamic> body) async {
+    final url = Uri.parse("$baseUrl$endpoint");
+
+    try {
+      final response = await http.post(
+        url,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(body),
+      );
+      return response;
+    } catch (e) {
+      throw Exception("POST request failed: $e");
+    }
+  }
+
+  //GET
+  Future<http.Response> get(String endpoint) async {
+    final token = await secureStorage.read(key: "token");
+    final url = Uri.parse("$baseUrl$endpoint");
+
+    try {
+      final response = await http.get(
+        url,
+        headers: {
+          if (token != null) 'Authorization': 'Bearer $token',
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+      );
+      return response;
+    } catch (e) {
+      throw Exception("GET request failed: $e");
+    }
+  }
+
+  Future<http.Response> postWithToken(String url, dynamic body) async {
+    final token = await secureStorage.read(key: "token");
+
+    return http.post(
+      Uri.parse(baseUrl + url),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+      body: body is String ? body : json.encode(body),
+    );
+  }
+
+  //PUT
+
+  //DELETE
+}
